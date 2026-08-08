6df9b5d15beefbcc9217fbd80bcc45a26a9e5716
MeowSalty <MeowSalty@users.noreply.github.com>
2026-08-08 04:58:23 +0000

    deploy: 85c40324027cf31167eaec2946625685d0335f50

---
+++ next/assets/chunks/swimlanesDiagram-G3AALYLV.BbjA5LB4.js
@@ -0,0 +1,8 @@
+import{c as e,s as r}from"./flowDiagram-23GEKE2U.CBlEJNVh.js";import{_ as a}from"./theme.DJ3T11Cc.js";import"./chunk-5VM5RSS4.DrWjGEA3.js";import"./chunk-XXDRQBXY.C9SvXUtP.js";import"./chunk-VR4S4FIN.CcyD4yzU.js";import"./chunk-32BRIVSS.DMQkHq56.js";import"./framework.CwYPcpF1.js";var o=a(t=>`${r(t)}
+  .swimlane.cluster rect {
+    stroke: ${t.clusterBorder} !important;
+  }
+  [data-look="neo"].cluster rect {
+    filter: none;
+  }
+`,"getStyles"),s=o,f=e({defaultLayout:"swimlane",styles:s});export{f as diagram};
